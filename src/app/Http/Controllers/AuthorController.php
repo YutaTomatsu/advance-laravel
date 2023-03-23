c4e09dc3322d66219f87bed209acdb1ebf148f18
Yuta <pkmn6106@gmail.com>
2023-03-23 15:21:12 +0900

    first commit

---
+++ src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+
+    public function add(){
+        return view('add');
+    }
+
+    public function create(Request $request){
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+
+    public function edit(Request $request){
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+}
